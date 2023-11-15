58099638521d2f75f65a2d5e7afca0df6bfa851d
Ayush Sharma <ayushsharma830230@gmail.com>
2023-11-15 22:58:47 +0530

    custom react

---
+++ index.js
@@ -0,0 +1,22 @@
+function reactRender(reactElement, Container) {
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    Container.appendChild(domElement)
+}
+
+const mainContainer = document.querySelector('#root')
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: "Click To Visit Google."
+}
+
+reactRender(reactElement, mainContainer)
